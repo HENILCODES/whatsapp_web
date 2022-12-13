321226201e2de20cb294bdb712e12f9402ecb3d3
henilcode <henilcode@gmail.com>
2022-12-13 19:37:41 +0530

    edit some

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React from 'react'
-import Leftside from './Leftside'
-import Rightside from './Rightside'
-export default function App() {
-  
+import React from "react";
+import './App.css'
+function App() {
   return (
-    <div className="main-box">
-      <div className='box'>
-        <Leftside />
-      </div>
-      <div className="w-100">
-        <Rightside />
+    <div className="main">
+      <div className="box">
+        <div className="infor">
+          <form autoComplete="off">
+            <div className="input_box">
+              <label htmlFor="user" className="label">
+                User name
+              </label>
+              <input
+                type="text"
+                className="input"
+                placeholder="type username"
+                id="user"
+                title="User Name"
+              />
+            </div>
+            <div className="input_box">
+              <label className="label" htmlFor="password">
+                password
+              </label>
+              <div className="password">
+                <input
+                  type="text"
+                  className="inputx"
+                  placeholder="type Password"
+                  id="password"
+                  title="Password"
+                />
+              </div>
+            </div>
+            <button className="Log_Button" type="submit">
+              Log in
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-
-  )
+  );
 }
